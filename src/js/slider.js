952c552cc06f22fb85e5ee93e685eca4436b8e62
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2022-04-04 20:55:13 +0300

    s

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,75 @@
-console.log('slider');
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination-feedback',
-      clickable: true,
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-      clickable: true,
+const slider_about = {
+  direction: 'horizontal',
+  loop: true,
 
-    },
-    slidesPerView: 3,
-    spaceBetween: 30,
-          
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        // when window width is >= 480px
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 15
-        },
-        // when window width is >= 769px
-        769: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-    }
-  });
+  pagination: {
+    el: '.swiper-pagination-about',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-about-btn-prev',
+    prevEl: '.swiper-about-btn-next',
+    clickable: true,
+
+  },
+  slidesPerView: 5,
+  spaceBetween: 30,
+        
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      // when window width is >= 769px
+      769: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      }
+  }
+}
+const slider_feedback = {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination-feedback',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    clickable: true,
+
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+        
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      // when window width is >= 769px
+      769: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+  }
+}
+// const swiper = new Swiper('.swiper', slider_feedback);
+
+const swiper1 = new Swiper('.swiper-about', slider_about);
